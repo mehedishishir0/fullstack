4bb40ca5855dcb47fabd177774309837b000f015
mehedi hasan shishir <mehedihasanshishir.info@gmail.com>
2026-04-05 12:54:53 +0600

    solve cors error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000","https://buddyscript-six.vercel.app"],
   credentials: true,
 }));
 
